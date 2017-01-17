da9c9992243d76ba907e41052b5c9b5059b946ce
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-01-17 18:32:55 +0000

    Create php for login and signup

---
+++ registrazione.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nome = $_POST["nome"];
+	$cognome = $_POST["cognome"];
+	$giorno = $_POST["giorno"];
+	$mese = $_POST["mese"];
+	$anno = $_POST["anno"];	
+	$username = $_POST["username"];
+	$password = $_POST["password"];
