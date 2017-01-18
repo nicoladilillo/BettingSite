cad4d0f7864f39ed58fc7fa9d87ef9e351ef1c69
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-01-19 00:46:31 +0100

    Create logout

--- index.php
+++ index.php
@@ -16,5 +16,15 @@
 
     <h1>Superenalotto</h1>
 
+    <?php
+      if(isset($_SESSION['login_user'])) {
+        echo "Ciao ".$_SESSION['login_user'];
+        echo "<a href='logout.php'>Esci</a>";
+      }
+      else {
+        echo "<a href='login.html'>Login</a>";
+      }
+    ?>
+
   </body>
 <html>
