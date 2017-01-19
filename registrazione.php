743faac1f465b1df0f6f2667a57905a52bc5a5bc
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-01-19 18:16:31 +0100

    Complete Registration

--- registrazione.php
+++ registrazione.php
@@ -1,9 +1,33 @@
 <?php
 
-    $nome = $_POST["nome"];
+  $nome = $_POST["nome"];
 	$cognome = $_POST["cognome"];
 	$giorno = $_POST["giorno"];
 	$mese = $_POST["mese"];
-	$anno = $_POST["anno"];	
+	$anno = $_POST["anno"];
 	$username = $_POST["username"];
 	$password = $_POST["password"];
+  $confermapassword = $_POST["confermapassword"];
+
+  include_once('connessione.php');
+
+  if($password == $confermapassword) {
+    $sql = "SELECT password
+            FROM User
+            WHERE username = '$username'";
+    $sth = $db->query($sql);
+    $row = $sth->fetchAll(PDO::FETCH_OBJ);
+    if ($row) {
+      echo "L'username esiste già";
+     }
+    else {
+      $sql =("INSERT INTO User (Nome, Cognome, giorno, mese, anno, username, password)
+              VALUES ('$nome', '$cognome', '$giorno', '$mese', '$anno', '$username', '$password')");
+      $sth = $db->query($sql);
+
+     header("location: index.php");
+    }
+  }
+  else {
+    echo "Le password sono diverse";
+  }
